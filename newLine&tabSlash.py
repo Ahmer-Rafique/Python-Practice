f20948702a4c54f6f9f7e41803b5659d8ee9d5d9
AhmerAli17 <ahmerali1771@gmail.com>
2024-02-07 12:29:11 +0000

    newLine and Tabe space Practice

---
+++ newLine&tabSlash.py
@@ -0,0 +1,14 @@
+name:str="Hy my name is Ahmer ali . How are you?"
+print("original = " , name);
+name1:str="Hy my name is Mamoon Ali.\nHow are you?"
+print("New Line = ", name1)
+name2:str="Hy my name is Hasnian .\tHow are you?"
+print("Tab Spase = ", name2)
+f_Name:str="Hy my name is Abbas";
+l_Name:str="How Are you?"
+d_Name:str=f" orignal Form  = {f_Name} {l_Name}"
+e_Name:str=f" New Line Form = {f_Name} \n{l_Name}"
+g_Name:str=f" Tab  Form  = {f_Name} \t{l_Name}"
+print(d_Name)
+print(e_Name)
+print(g_Name)
