c9b806fb194f4809210da5f2e67f6e2d0c8c4730
AhmerAli17 <ahmerali1771@gmail.com>
2024-02-07 15:01:26 +0000

    F-String practice

---
+++ f_String.py
@@ -0,0 +1,9 @@
+name:str="Ahmer Ali";
+age:int=18;
+edu:str="Graduate"
+hobbies:list=["Cricket" ,"Coding" , "Programing"]
+data:complex=f"""My name is = {name}
+My age is = {age} 
+My Education is = {edu}
+My hobbies is = {hobbies} """;
+print(data)
