1fd4ace7cbe1d373dc0a46da9020d6691d3038a5
AhmerAli17 <ahmerali1771@gmail.com>
2024-02-07 12:12:52 +0000

    concatinate method

---
+++ concatinateMethod.py
@@ -0,0 +1,6 @@
+first_name:str="Ahmer";
+last_name:str="Ali";
+full_name:str=f"{first_name} {last_name}"
+print("First Name =" , first_name);
+print("Last Name =" , last_name);
+print("Full Name =" , full_name);
