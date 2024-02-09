ea8fd252324ea6d953cd85a3253be847aa22365b
AhmerAli17 <ahmerali1771@gmail.com>
2024-02-09 11:10:17 +0000

    Formate Method

---
+++ formatMethod.py
@@ -0,0 +1,9 @@
+#name:str= "my name is {} . My age is {}".format("Ahmer Ali" , 18);
+#print(name);
+#name1:str= "my name is {0} . My age is {1}".format("Mamoon" , 16);
+#print(name1);
+#name2:str= "my name is {namew} . My age is {age}".format(namew="Ahmer Ali" , age=18);
+#print(name2);
+usernput:str=input("please enter your name");
+name:str="my name is {}".format(usernput);
+print(name);
