8d37c88f4519a5e4bb245522678ae728b892948c
AhmerAli17 <ahmerali1771@gmail.com>
2024-02-09 11:53:47 +0000

    Number Method

---
+++ numberMethod.py
@@ -0,0 +1,7 @@
+num1:int=15
+num2:int=10
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+print(f"{num1} x {num2} = {num1 * num2}")
+print(f"{num1} / {num2} = {num1 / num2}")
+print(f"{num1} % {num2} = {num1 % num2}")
