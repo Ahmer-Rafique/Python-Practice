be06c8aaeb845d8b20522b72a3c706228e721306
AhmerAli17 <ahmerali1771@gmail.com>
2024-02-24 15:51:45 +0000

    Logical operators

---
+++ logicalOperators.py
@@ -0,0 +1,11 @@
+a:int=10;
+b:int=20;
+# And Operators
+print(a==10 and b>a) # Answer is true
+print(a==20 and b>a) # Answer is false 
+# Or Operators 
+print(a==20 or b>a) # Answer is true
+print (a==20 or b<a) # Answer is False
+# not Operators
+print(not a==20) # Answer is true 
+print(not b==20) # Answer is False 
