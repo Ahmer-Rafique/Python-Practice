48fd8726341adec9cda09179045594e865665ed3
AhmerAli17 <ahmerali1771@gmail.com>
2024-03-05 16:21:47 +0000

    add Methods

--- listMethod.py
+++ listMethod.py
@@ -1,3 +1,16 @@
 a:list=["Ahmer Ali",18 ,["Cricket", "Progrming" ,"Coding"] ]
 print(a)
+# To Find Length
 print(len(a))
+# To Find Type
+print(type(a))
+# Find By Positive index
+print(a[2])
+# To Find By Negitive index
+print(a[-2])
+# To find  between Numbers Positive
+print(a[:3])
+# To Find between Number by Negitive index
+print(a[-3:-1])
+# To find Skip
+print(a[0:3:2])
