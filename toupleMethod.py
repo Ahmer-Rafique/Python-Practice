223a6a16cdf781ef741199bd05b7582ab5d7e13b
AhmerAli17 <ahmerali1771@gmail.com>
2024-03-08 15:46:31 +0000

    add methods in tuple

--- toupleMethod.py
+++ toupleMethod.py
@@ -1,2 +1,9 @@
-a= ("Ahmer", "Ali" , 12 , ["This is a list"])
+a= ("Ahmer", "Ali" , 12 , True,["This is a list"])
 print(a);
+# methods are we applied in touple
+print(len(a))
+print(type(a))
+print(a[1])
+print(a[-3])
+print(a[:4])
+print(a[0:2:2])
