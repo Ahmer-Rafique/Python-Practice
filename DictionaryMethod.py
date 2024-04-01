2332740601ad974390a3bba5bb7bda739fa5b3fa
AhmerAli17 <ahmerali1771@gmail.com>
2024-04-01 12:33:55 +0100

    update Dictionary method

--- DictionaryMethod.py
+++ DictionaryMethod.py
@@ -12,3 +12,18 @@ print(set_data["age"])
 set_data["age"] = 19
 set_data["adat"] = "nice"
 print(set_data);
+# Constructor Form of Dictionary Method
+set_data1 = dict(
+    name = "Mamoon",
+    age = 16,
+     hobbbies=["Criicket","Coding", "Programing"]
+)
+print(set_data1)
+print(len(set_data1))
+print(type(set_data1))
+print(set_data1["name"])
+print(set_data1["hobbbies"])
+print(set_data1["age"])
+set_data1["age"] = 19
+set_data1["adat"] = "nice"
+print(set_data1);
