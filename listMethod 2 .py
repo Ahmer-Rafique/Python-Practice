327830c81fcdde1bd376d454abe93fd6521005dd
AhmerAli17 <ahmerali1771@gmail.com>
2024-04-01 10:48:49 +0100

    update listmaethod

--- listMethod 2 .py
+++ listMethod 2 .py
@@ -1,11 +1,11 @@
  # Sort Method 
-a =["a","b" ,"c", "d" ,"d","a"]
-print(a);
-a.sort(reverse=True)
-print(a)
-# Count Method
-d = a.count("a")
-print(d)
-# * method
-name =["Ahmer ","is","Developer"]
-print(*name)
+a =["a","b" ,"c", "d" ,"d","a"] 
+print(a); 
+a.sort(reverse=True) 
+print(a) 
+# Count Method 
+d = a.count("a")  
+print(d)  
+# * method 
+name =["Ahmer","is","a","Developer"] 
+print(*name);
