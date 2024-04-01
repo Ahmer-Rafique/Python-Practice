72303f6e83366381dfc1502c50b447dcb3dbb70b
AhmerAli17 <ahmerali1771@gmail.com>
2024-04-01 12:26:28 +0100

    update Doctionary method

--- DictionaryMethod.py
+++ DictionaryMethod.py
@@ -5,3 +5,10 @@ set_data ={
 }
 print(set_data)
 print(len(set_data))
+print(type(set_data))
+print(set_data["name"])
+print(set_data["hobbbies"])
+print(set_data["age"])
+set_data["age"] = 19
+set_data["adat"] = "nice"
+print(set_data);
